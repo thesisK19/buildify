ca2db6abe403110091ac09374590213e479593d7
y.dtx <y.dtx@teko.vn>
2023-05-04 01:44:40 +0700

    make update

--- app/file_mgt/internal/handler/middleware.go
+++ app/file_mgt/internal/handler/middleware.go
@@ -1,11 +1,37 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
 // JSONContentTypeMiddleware will add the json content type header for all endpoints
 func JSONContentTypeMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("content-type", "application/json; charset=UTF-8")
+		next.ServeHTTP(w, r)
+	})
+}
+
+func Recovery(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			err := recover()
+			if err != nil {
+				log.Println(err)
+
+				jsonBody, _ := json.Marshal(map[string]string{
+					"error": "There was an internal server error",
+				})
+
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusInternalServerError)
+				w.Write(jsonBody)
+			}
+
+		}()
+
 		next.ServeHTTP(w, r)
 	})
 }
